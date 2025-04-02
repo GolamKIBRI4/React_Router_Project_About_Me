7c46d9c04289a220d8c7a8113c5a9538fcd46e14
GolamKIBRI4 <gk599625@gmail.com>
2025-04-03 00:34:13 +0600

    Nested Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import "./App.css";
-import { createBrowserRouter, createRoutesFromElements, Route,RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
-import RootLayout from "./Layout/RootLayout";
 
+import RootLayout from "./Layout/RootLayout";
+import ContactLayout from "./Layout/ContactLayout";
+import ContactInfo from "./Components/ContactInfo";
+import ContactForm from "./Components/ContactForm";
 
 function App() {
-  const router = createBrowserRouter(createRoutesFromElements(
-   <Route path="/" element={<RootLayout/>}>
-          <Route index element={<Home/>} />
-          <Route path="Products" element={<Products/>} />
-          <Route path="About" element={<About/>} />
-          <Route path="Contact" element={<Contact/>} />
-   </Route> 
-  ))
-  return ( 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="Products" element={<Products />} />
+        <Route path="About" element={<About />} />
+        <Route path="Contact" element={<ContactLayout />}>
+          <Route path="contactInfo" element={<ContactInfo />} />
+          <Route path="contactForm" element={<ContactForm />} />
+        </Route>
+      </Route>
+    )
+  );
+  return (
     <>
-       <RouterProvider router={router} />
-      
+      <RouterProvider router={router} />
     </>
   );
 }
---
+++ src/Components/ContactInfo.tsx
@@ -0,0 +1,16 @@
+
+const ContactInfo = () => {
+  return (
+    <div>
+        <div>
+            Name:Abul,
+            <br />
+            Email:abul1234@gmail.com
+            <br />
+            Addtress:adjandja,jadnja,djadn
+        </div>
+    </div>
+  )
+}
+
+export default ContactInfo
