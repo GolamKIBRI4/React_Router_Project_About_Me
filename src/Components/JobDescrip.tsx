4bc20f985cfac07985a2d544b3a2776daec031a3
GolamKIBRI4 <gk599625@gmail.com>
2025-04-11 14:30:46 +0600

    Job description text expandfeature

---
+++ src/Components/JobDescrip.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+interface Props {
+  description: string;
+  MaxWord?: number;
+}
+const JobDescrip = ({ description, MaxWord = 20 }: Props) => {
+  const [isExpand, setIsExpand] = useState(false);
+
+  if (description.length <= MaxWord) return <p>{description}</p>;
+
+  const text = isExpand ? description : description.substring(0, MaxWord);
+
+  return (
+    <p>
+      {text}...
+      <button className="btn" onClick={() => setIsExpand(!isExpand)}>
+        {isExpand ? "Less" : "More"}
+      </button>
+    </p>
+  );
+};
+
+export default JobDescrip;
--- src/pages/Jobs.tsx
+++ src/pages/Jobs.tsx
@@ -4,23 +4,22 @@ export interface Job {
   title: string;
   Salary: number;
   Location: string;
+  description: string;
 }
 
 const Jobs = () => {
   const jobData = useLoaderData() as Job[];
   return (
-    
-      <div className="jobs">
+    <div className="jobs">
       {jobData.map((job) => {
-        return <Link to={job.id.toString()} key={job.id}>
-        <h4>{job.title}</h4>
-        <p>{job.Location}</p>
-        </Link>
-        
-})}
+        return (
+          <Link to={job.id.toString()} key={job.id}>
+            <h4>{job.title}</h4>
+            <p>{job.Location}</p>
+          </Link>
+        );
+      })}
     </div>
-    
-  
   );
 };
 
